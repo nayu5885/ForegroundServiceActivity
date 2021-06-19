e120412fb178190cdc4483b2e1d351669aa3ce0f
lifeistech <happymmw5885@gmail.com>
2021-06-19 18:55:23 +0900

    ManifestにForegroundService権限とサービスクラス名を追加し無事起動

--- app/src/main/java/com/example/foregroundserviceactivity/MainActivity.kt
+++ app/src/main/java/com/example/foregroundserviceactivity/MainActivity.kt
@@ -3,20 +3,21 @@ package com.example.foregroundserviceactivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val serviceIntent = Intent(this, MyForegroundService::class.java)
-        startForegroundService(serviceIntent)
+        Log.d("tag","start")
 
         val button: Button =findViewById(R.id.button)
 
         button.setOnClickListener {
-
+            val serviceIntent = Intent(this, MyForegroundService::class.java)
+            startForegroundService(serviceIntent)
         }
     }
 }
